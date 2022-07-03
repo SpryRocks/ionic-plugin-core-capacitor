c7b14002bb0feadeaa57a7bf2539f5f206ac06a9
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2022-07-03 15:21:16 +0200

    Fix Factory

--- src/Factory.ts
+++ src/Factory.ts
@@ -1,13 +1,5 @@
-import * as cap from '@capacitor/core';
+import {registerPlugin} from '@capacitor/core';
 
 export const createPlugin = <TPlugin>(pluginName: string): TPlugin => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  if (cap.registerPlugin) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    return cap.registerPlugin<TPlugin>(pluginName);
-  } else {
-    return cap.Plugins[pluginName] as TPlugin;
-  }
+  return registerPlugin<TPlugin>(pluginName);
 };

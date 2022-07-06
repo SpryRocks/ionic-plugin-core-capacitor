8646198aeb27a2bd5b37e28eb7ad70fdbdd59d43
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2022-07-06 22:10:08 +0200

    Refactoring

--- android/src/main/java/com/ionic/plugin/android/capacitor/core/Plugin.kt
+++ android/src/main/java/com/ionic/plugin/android/capacitor/core/Plugin.kt
@@ -2,9 +2,12 @@ package com.ionic.plugin.android.capacitor.core
 
 import com.ionic.plugin.android.capacitor.core.actions.Delegate
 import com.ionic.plugin.android.capacitor.core.actions.WrapperDelegate
+import java.util.concurrent.Executors
 
 abstract class Plugin<TActionKey, TDelegate : Delegate> :
-    com.ionic.plugin.android.core.Plugin<TActionKey, TDelegate>() {
+    com.ionic.plugin.android.core.Plugin<TActionKey, TDelegate>(), Delegate {
+    override val threadPool = Executors.newCachedThreadPool()!! // move to wrapper
+
     internal fun initialize(wrapperDelegate: WrapperDelegate) {
         super.initialize(wrapperDelegate)
     }
--- android/src/main/java/com/ionic/plugin/android/capacitor/core/actions/WrapperDelegate.kt
+++ android/src/main/java/com/ionic/plugin/android/capacitor/core/actions/WrapperDelegate.kt
@@ -1,4 +1,3 @@
 package com.ionic.plugin.android.capacitor.core.actions
 
-interface WrapperDelegate : com.ionic.plugin.android.core.actions.WrapperDelegate {
-}
+interface WrapperDelegate : com.ionic.plugin.android.core.actions.WrapperDelegate

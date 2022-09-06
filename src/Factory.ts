c2b22838c6dcb58c5989c6ee3f31f921c16261f6
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2022-09-06 12:22:15 +0200

    Refactoring

--- src/Factory.ts
+++ src/Factory.ts
@@ -2,8 +2,8 @@ import {CreatePlugin, PluginRegistration} from './IFactory';
 import {Plugins} from '@capacitor/core';
 
 export const createPlugin: CreatePlugin = <TPlugin>(
-    pluginName: string,
-    options?: {web?: PluginRegistration},
-    ): TPlugin => {
+  pluginName: string,
+  _?: {web?: PluginRegistration},
+): TPlugin => {
   return Plugins[pluginName] as TPlugin;
 };

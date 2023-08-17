ce60be05975f46bac26a52873f6dbe165bff45d5
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-08-17 15:16:13 +0200

    Refactoring

--- src/Factory.ts
+++ src/Factory.ts
@@ -2,16 +2,14 @@ import {CreatePlugin, PluginRegistration} from './IFactory';
 import {IDefinitions, PluginProxy} from './definitions';
 import {registerPlugin} from '@capacitor/core';
 
+type CreatePluginOptions = {web?: PluginRegistration; cap2?: {pluginName: string}};
+
 export const createPlugin: CreatePlugin = <TPlugin>(
   pluginName: string,
-  options?: {web?: PluginRegistration},
-): TPlugin => {
-  return registerPlugin<TPlugin>(pluginName, {web: options?.web});
-};
+  options?: CreatePluginOptions,
+): TPlugin => registerPlugin<TPlugin>(pluginName, {web: options?.web});
 
 export const createPluginProxy = <TDefinitions extends IDefinitions>(
   pluginName: string,
-  options?: {
-    web?: PluginRegistration;
-  },
+  options?: CreatePluginOptions,
 ) => createPlugin<PluginProxy<TDefinitions>>(pluginName, options);

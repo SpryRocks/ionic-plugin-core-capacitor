dc99e6e6e8dbd64195e7a30afdab8acb0320ce2f
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2024-03-01 14:08:17 +0100

    Remove threading helpers

--- ios/src/helpers/ThreadingHelpers.swift
+++
@@ -1,9 +0,0 @@
-public func runInMainThreadAsync<T>(_ action: @escaping () async throws -> T) async throws -> T {
-    return try await withCheckedThrowingContinuation { c in
-        DispatchQueue.main.async {
-            Task {
-                c.resume(returning: try await action())
-            }
-        }
-    }
-}
